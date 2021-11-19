ec3a79c24b499f899176cce8b069b49544fb2a8e
trung-fullstack <trungnfullstack@gmail.com>
2021-11-19 14:11:59 -0600

    feat(server): add rot13 conversion

--- packages/server/services/utils.ts
+++ packages/server/services/utils.ts
@@ -1,6 +1,8 @@
 export class Utils {
   public convertString(str: string): string {
-    return str
+    return str.replace(/[a-z]/gi, x => {
+      return String.fromCharCode(x.charCodeAt(0) + (x.toLowerCase() <= 'm' ? 13 : -13));
+    });
   }
 }
 
